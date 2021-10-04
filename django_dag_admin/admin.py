6a76feb72264a57945d85c0f73885d6e2a6e0fd4
Paul Gammans <pgammans@gammascience.co.uk>
2021-10-04 08:54:42 +0100

    fix: selection f single node in admin.
    prevent the full query with sorting and sequencing being
    used for single node selection

--- django_dag_admin/admin.py
+++ django_dag_admin/admin.py
@@ -37,6 +37,22 @@ class DjangoDagAdmin(admin.ModelAdmin):
         )
         return filter(None, actions)
 
+
+    def get_object(self, request, object_id, from_field=None):
+        """
+        Return an instance matching the field and value provided, the primary
+        key is used if no field is provided. Return ``None`` if no match is
+        found or the object_id fails validation.
+        """
+        queryset = super().get_queryset(request, )
+        model = queryset.model
+        field = model._meta.pk if from_field is None else model._meta.get_field(from_field)
+        try:
+            object_id = field.to_python(object_id)
+            return queryset.get(**{field.name: object_id})
+        except (model.DoesNotExist, ValidationError, ValueError):
+            return None
+
     def get_changelist(self, request, **kwargs):
         """
         Return the ChangeList class for use on the changelist page.
